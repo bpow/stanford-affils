e4e9393d6051b28486314990dcccadbd203da44e
Liam Mulhall <liammulh@gmail.com>
2024-03-28 16:28:09 -0700

    chore: Set up logging
    Set up a logger and configure logging on AWS.
    
    For:
    https://github.com/ClinGen/affils/issues/11

---
+++ doc/logs.md
@@ -0,0 +1,7 @@
+# Logs
+
+When running locally, logs are written to stdout. When running on AWS,
+logs can be viewed in CloudWatch. To view the logs, navigate to the
+CloudWatch web console, then in the side bar click the link to the log
+groups page. Then look for the `web.stdout.log` log group. Then click
+the appropriate log stream (probably the most recent one).
--- src/app.py
+++ src/app.py
@@ -4,11 +4,16 @@ Set up API routes.
 """
 
 # Built-in libraries:
+import logging
 import os
 import subprocess
 
 # Third-party dependencies:
 from flask import Flask
+
+# Configure logger.
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -16,12 +21,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """A cheeky hello world route."""
+    logger.info("User accessed /")
     return "<p>Look on my Affiliations Service, ye Mighty, and despair!</p>"
 
 
 @app.route("/sha")
 def current_git_sha():
     """Displays current Git SHA."""
+    logger.info("User accessed /sha")
     command = ["git", "rev-parse", "HEAD"]
     output = subprocess.run(command, check=False, capture_output=True).stdout.decode(
         "utf-8"
@@ -34,4 +41,5 @@ def current_git_sha():
 @app.route("/env")
 def display_env_var():
     """Displays environment variable for testing purposes."""
+    logger.info("User accessed /env")
     return os.environ.get("SOME_VAR")
