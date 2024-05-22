83b5af9d5fed489d64be1efeadfacc2b0f90f051
Liam Mulhall <liammulh@gmail.com>
2024-05-22 15:34:53 -0700

    chore: Fix lint errors in `tasks.py`

--- tasks.py
+++ tasks.py
@@ -4,6 +4,14 @@ All command line tasks should be defined in this file. The only
 exception to this is managing dependencies via Pipenv.
 """
 
+# Invoke always requires a context parameter, even if it ends up going
+# unused. As of this writing, there are a handful of tasks that don't
+# use their context parameters.
+# pylint: disable=unused-argument
+
+# Built-in libraries:
+import sys
+
 # Third-party dependencies:
 from dotenv import dotenv_values
 from invoke import task
@@ -20,13 +28,13 @@ ACTUAL_CONF = dotenv_values(ENV_ACTUAL)
 @task
 def fmt(c):
     """Format code."""
-    c.run(f"black .")
+    c.run("black .")
 
 
 @task
 def lint(c):
     """Run the linter."""
-    c.run(f"pylint src")
+    c.run("pylint src")
 
 
 @task
@@ -90,12 +98,14 @@ def envsame(c):
     """Ensure environment variable keys match."""
     if TEMPLATE_CONF.keys() != ACTUAL_CONF.keys():
         print(".env keys do not match. Check your .env files.")
-        exit(1)
+        sys.exit(1)
 
 
 @task(pre=[fmt, lint, types, test, sqlfmt, sqlfix, sqllint, envsame])
 def check(c):
     """Run all code checks."""
+    # Also lint this file.
+    c.run("pylint tasks.py")
 
 
 @task
