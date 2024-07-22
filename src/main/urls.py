63a076b308cd8a3c94c5fd4a2e856305579f0ff8
sanchegm <sanchegm@stanford.edu>
2024-07-22 13:23:26 -0700

    update urls.py (#90)
    Remove "admin/" path and refactor so that base path leads to the built in admin site.

--- src/main/urls.py
+++ src/main/urls.py
@@ -9,7 +9,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("affiliations.urls")),
-    path("admin/", admin.site.urls),
+    path("", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
 ]
