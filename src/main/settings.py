6f0385023cb17758232ff9cf0df437ecf3b8c2b2
sanchegm <sanchegm@stanford.edu>
2024-10-15 12:20:03 -0700

    change back region syntax

--- src/main/settings.py
+++ src/main/settings.py
@@ -210,7 +210,7 @@ logger_boto3_session = boto3.client(
     "logs",
     aws_access_key_id=os.environ.get("AFFILS_AWS_ACCESS_KEY"),
     aws_secret_access_key=os.environ.get("AFFILS_AWS_SECRET_KEY"),
-    region_name=os.environ.get("AFFILS_AWS_REGION"),
+        region_name="us-west-2",
 )
 
 LOGGING = {
