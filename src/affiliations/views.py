20767bfb8a912cc96ef5ccaa4c6294c375ea84da
sanchegm <sanchegm@stanford.edu>
2024-11-13 15:06:18 -0800

    Update views.py detail view
    - Add function to return JSON response of specific non-deleted affiliation(s) in the current JSON file format.

--- src/affiliations/views.py
+++ src/affiliations/views.py
@@ -68,3 +68,46 @@ def affiliations_list_json_format(request):  # pylint: disable=unused-argument
     return JsonResponse(list(response_obj.values()), status=200, safe=False)
 
 
+@api_view(["GET"])
+def affiliation_detail_json_format(request):
+    """List specific affiliation in old JSON format."""
+    affil_id = request.GET.get("affil_id")
+    affils_queryset = Affiliation.objects.filter(
+        affiliation_id=affil_id, is_deleted=False
+    ).values()
+    response_obj = {}
+    for affil in affils_queryset:
+        affil_type = affil["type"].lower()
+        if affil["affiliation_id"] not in response_obj:
+            old_json_format = {
+                "affiliation_id": affil["affiliation_id"],
+                "affiliation_fullname": affil["full_name"],
+                "subgroups": {
+                    affil_type: {
+                        "id": affil["expert_panel_id"],
+                        "fullname": affil["full_name"],
+                    },
+                },
+                "approver": [],
+            }
+            response_obj[affil["affiliation_id"]] = old_json_format
+
+        elif affil_type not in response_obj[affil["affiliation_id"]]["subgroups"]:
+            response_obj[affil["affiliation_id"]]["affiliation_fullname"] = (
+                response_obj[affil["affiliation_id"]]["affiliation_fullname"]
+                + "/"
+                + affil["full_name"]
+            )
+            response_obj[affil["affiliation_id"]]["subgroups"][affil_type] = {
+                affil_type: {
+                    "id": affil["expert_panel_id"],
+                    "fullname": affil["full_name"],
+                },
+            }
+        approvers_queryset = Approver.objects.filter(
+            affiliation_id=affil["id"]
+        ).values_list("approver_name", flat=True)
+        for name in approvers_queryset:
+            response_obj[affil["affiliation_id"]]["approver"].append(name)
+
+    return JsonResponse(list(response_obj.values()), status=200, safe=False)
