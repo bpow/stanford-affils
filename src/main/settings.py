8c4ad1875f8e2cb8aa01dcfc775a8f2327c4bbf7
sanchegm <sanchegm@stanford.edu>
2024-11-25 17:47:05 -0800

    Add django-cors-headers

--- src/main/settings.py
+++ src/main/settings.py
@@ -36,6 +36,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["affils.clinicalgenome.org", "affils-test.clinicalgenome.org"]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
+
 CSRF_TRUSTED_ORIGINS = [
     "https://affils.clinicalgenome.org",
     "https://affils-test.clinicalgenome.org",
@@ -58,9 +62,11 @@ INSTALLED_APPS = [
     "django_extensions",
     "dbbackup",  # django-dbbackup
     "django_crontab",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
